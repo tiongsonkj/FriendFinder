cc228ef970bd60f4dbec5da0fc375fe755834b66
Kelvin Tiongson <kelvintiongson30@gmail.com>
2017-12-09 07:58:14 -0600

    added extra characters in data

--- app/data/friends.js
+++ app/data/friends.js
@@ -9,11 +9,82 @@ var friends = [
 		5,
 		3,
 		5,
+		1,
 		2,
 		5,
+		4,
+		1,
+		5]
+	}, {
+		"name": "Patrick Star",
+		"photo": "https://vignette.wikia.nocookie.net/spongebob/images/3/33/Patrick_Star.svg/revision/latest/scale-to-width-down/270?cb=20100724183918",
+		"scores":[
+		3,
+		4,
+		3,
+		5,
+		3,
+		2,
+		3,
+		2,
+		1,
+		3]
+	}, {
+		"name": "Sandy Cheeks",
+		"photo": "https://vignette.wikia.nocookie.net/spongebob/images/a/a0/Sandy_Cheeks.svg/revision/latest/scale-to-width-down/270?cb=20100920201409",
+		"scores":[
+		3,
+		3,
+		4,
+		2,
+		5,
+		3,
+		4,
 		2,
 		1,
 		5]
+	}, {
+		"name": "Squidward Tentacles",
+		"photo": "https://upload.wikimedia.org/wikipedia/en/a/ac/Squidward.png",
+		"scores":[
+		5,
+		1,
+		2,
+		5,
+		1,
+		3,
+		1,
+		5,
+		1,
+		1]
+	}, {
+		"name": "Mr. Krabs",
+		"photo": "https://img.buzzfeed.com/buzzfeed-static/static/2017-05/17/11/asset/buzzfeed-prod-fastlane-01/sub-buzz-24051-1495034027-18.jpg?downsize=715:*&output-format=auto&output-quality=auto",
+		"scores":[
+		3,
+		3,
+		5,
+		5,
+		3,
+		5,
+		4,
+		1,
+		3,
+		1]
+	}, {
+		"name": "Plankton",
+		"photo": "https://vignette.wikia.nocookie.net/nickelodeon/images/d/d3/Plankton_by_wombat7500-d417kmu.png/revision/latest?cb=20160918215953",
+		"scores":[
+		5,
+		2,
+		2,
+		5,
+		1,
+		4,
+		4,
+		1,
+		5,
+		2]
 	}
 ];
 
